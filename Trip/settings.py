3c57fbc57f5cc16de8ef6c06d27d495edeb00b6c
tongchengbin <58296672@qq.com>
2019-03-19 18:20:40 +0800

    map

--- Trip/settings.py
+++ Trip/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n+)fcatp&ttksd7*-)#fbt1d7809+_5$ndynw5a4@itx-&f!b^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["0.0.0.0","*"]
 
